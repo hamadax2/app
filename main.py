8d5762783c99a3fb4f5d5cb44e77e93731c75038
Mohamed Sulieman Alomda <44388599+hamadax2@users.noreply.github.com>
2023-11-30 04:44:20 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+import kivy
+from kivy.core.clipboard import Clipboard
+from kivymd.app import MDApp
+from kivymd.uix.dialog import MDDialog
+from kivy.lang import Builder
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.navigationdrawer import MDNavigationDrawer
+import requests
+import threading
+from kivy.clock import mainthread
+
+
+        
+class SdApp(MDApp):
+    def build(self):
+        self.title = "Subdomain-Finder"
+        self.theme_cls.theme_style = "Light"   
+        self.theme_cls.primary_palette = "Red"
+        self.theme_cls.hue="700"
+        return Builder.load_file("sub.kv") 
+        
+    @mainthread
+    def search(self, text):
+        t1 = threading.Thread(target=self.find, args=(text,), daemon=True)
+        t1.start()
+
+    def find(self, text):
+        domain = self.root.ids.domain.text
+        self.root.ids.rc_spin.active = True
+       # if domain == "":
+#            enter_domain = MDDialog(title="error" ,text='Please enter a domain')
+        try:
+            r = requests.get("http://api.hackertarget.com/hostsearch/?q=" +domain)
+            if r.status_code == 200:
+                results = r.text.split("\n")
+                with open(f"{domain}-sub.txt","w") as file:
+                    for result in results:
+                        subdomain = result.split(",")[0]
+                        file.write(subdomain+"\n")
+            with open(f"{domain}-sub.txt","r") as file:
+                data = file.read()
+                self.root.ids.subdomains.text = f"Subdomains of {domain} \n\n{data}"
+        except Exception as e:
+             print(e)
+             self.root.ids.subdomains.text = (
+                "Sorry unable to find "
+                + self.root.ids.domain.text
+            )
+        self.root.ids.rc_spin.active = False
+             
+        
+    def clear(self):
+        self.root.ids.subdomains.text = ""
+    def copy(self):
+        Clipboard.copy(self.root.ids.subdomains.text)
+    def dark(self):
+        self.theme_cls.theme_style = self.theme_cls.theme_style == "Dark" and "Light" or "Dark"
+        #self.message = "Switched to Light/Dark" 
+        #self.root.ids.mod.text = self.message
+        
+SdApp().run()
